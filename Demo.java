a6d6e9abf3dba2fe9abb957f22f98b47e29e56cc
Sushantpatil45 <2004sushantpatil@gmail.com>
2026-06-12 13:26:47 +0530

    Add new file

---
+++ Demo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Demo{
+    public static void main(String[] args) {
+        System.out.println("Enter the Your name : ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name);
+    }
+}
